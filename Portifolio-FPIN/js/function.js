9f8fe159286dfebefa0df6aae270d9a4bf5ae897
Filipe Esteves <filipe.esteves08@gmail.com>
2018-09-11 18:00:34 -0300

    Portifólio

---
+++ Portifolio-FPIN/js/function.js
@@ -0,0 +1,85 @@
+//////////////SLIDE
+
+var $item = $('.carousel-item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
+/////////////////
+
+///////////CONTADOR
+
+const tempo_intervalo = 5; //ms -> define a velocidade da animação
+const tempo = 10000; //ms -> define o tempo total da animaçao
+
+$('.counter-up').each(function() {  
+  let count_to = parseInt($(this).data('countTo'));
+  let intervalos = tempo / tempo_intervalo; //quantos passos de animação tem
+  let incremento = count_to / intervalos; //quanto cada contador deve aumentar
+  let valor = 0;
+  let el = $(this);
+  
+  let timer = setInterval(function() {
+    if (valor >= count_to){ //se já contou tudo tem de parar o timer
+      valor = count_to;
+      clearInterval(timer);
+    }
+    
+    let texto = valor.toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    el.text(texto);
+    valor += incremento;      
+  }, tempo_intervalo);
+});
+
+/////////////////////////
+
+/////////////////SCROLL
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+      $('.navbar').css("background-color", "#ffff")
+  } else {
+      $('.navbar').css("background-color", "#ffff")
+  }
+});
+
+///////////////////
+
+///////// MENU
+
+$(document).ready(function() {
+  // mostra esconde menu
+    $("a.mostra-esconde-menu").click(function(){
+        $(".menu_mluv_retratil").toggleClass('esconder');     
+    }); 
+  });
+  // menu ao rolar
+  $(window).bind('scroll', function () {
+        if ($(window).scrollTop() < 450) {
+          $(".menu_mluv_retratil").addClass('esconder');
+        } else if ($(window).scrollTop() > 450) {
+          $(".menu_mluv_retratil").removeClass('esconder');         
+        }
+  });
+
+  //////////////
